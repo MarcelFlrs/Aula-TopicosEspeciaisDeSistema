ce8bfb546c6901b538d7f7787dfd8be20e468d29
MarcelFlrs <127535882+MarcelFlrs@users.noreply.github.com>
2024-03-14 10:39:50 -0300

    .

---
+++ Ambientacao/Program.cs
@@ -0,0 +1,4 @@
+﻿Console.WriteLine("Digite a sua idade: ");
+string idade = Console.ReadLine();
+int i = Convert.ToInt16(idade);
+Console.WriteLine("Idade: " + i);
---
+++ ListaExercicio/Exe06B/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Exe06B;
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] vetor = new int[100];
+        Random aleatorio = new Random();
+        for (int i = 0; i < vetor.Length; i++)
+        {
+            vetor[i] = aleatorio.Next(100);
+        }
+
+        //Imprimir vetor antes da ordenação
+        for (int i = 0; i < vetor.Length; i++)
+        {
+            Console.Write(vetor[i] + " ");
+        }
+        Console.WriteLine("\n");
+
+        Array.Sort(vetor);
+        //Imprimir vetor depois da ordenação
+        for (int i = 0; i < vetor.Length; i++)
+        {
+            Console.Write(vetor[i] + " ");
+        }
+    }
+}
